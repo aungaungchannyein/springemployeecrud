fa0d0e6ebd10f763ee688134e19a168b67dae777
ChanNyein AungAung <aungaung.channyein@gmail.com>
2022-03-15 12:38:59 +0630

    first commit employee crud

---
+++ src/main/java/com/employees/employeeform/controller/EmployeeController.java
@@ -0,0 +1,73 @@
+package com.employees.employeeform.controller;
+
+import com.employees.employeeform.entity.Employee;
+import com.employees.employeeform.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel){
+
+        List<Employee> theEmployees= employeeService.findAll();
+
+        theModel.addAttribute("employees",theEmployees);
+
+
+        return "employees/list-employees";
+
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+
+    }
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+        employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
+
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+
+        Employee theEmployee = employeeService.findById(theId);
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId){
+
+        employeeService.deleteById(theId);
+
+        return "redirect:/employees/list";
+    }
+
+
+
+
+}
---
+++ src/main/java/com/employees/employeeform/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.employees.employeeform.service;
+
+import com.employees.employeeform.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+
+}
